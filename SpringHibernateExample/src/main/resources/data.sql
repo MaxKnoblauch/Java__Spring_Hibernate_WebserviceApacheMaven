1987899b4a070818d976db6428bfa391672e0a11
Max Knoblauch <knoblauchm.tin22@student.dhbw-heidenheim.de>
2024-10-03 17:56:18 +0200

    data.sql fügt Person/Objekt/Reservierung bei ausführen ein

--- SpringHibernateExample/src/main/resources/data.sql
+++ SpringHibernateExample/src/main/resources/data.sql
@@ -12,6 +12,18 @@ CREATE TABLE IF NOT EXISTS `objekt` (
     `name` VARCHAR(255)
 ) AUTO_INCREMENT = 100;
 
+-- Tabelle reservierung erstellen, wenn sie noch nicht existiert
+CREATE TABLE IF NOT EXISTS `reservierung` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `details` VARCHAR(255),
+    `end_datum` DATETIME,
+    `start_datum` DATETIME,
+    `objekt_id` INT,
+    `person_id` INT,
+    FOREIGN KEY (`objekt_id`) REFERENCES `objekt`(`id`),
+    FOREIGN KEY (`person_id`) REFERENCES `person`(`id`)
+) AUTO_INCREMENT = 100;
+
 -- Überprüfen, ob bereits Daten in der person-Tabelle existieren und nur hinzufügen, wenn sie nicht existieren
 INSERT INTO `person` (`age`, `name`, `vorname`)
 SELECT 25, 'Petra', 'Test' WHERE NOT EXISTS (SELECT 1 FROM `person` WHERE `name` = 'Petra' AND `vorname` = 'Test');
@@ -28,6 +40,11 @@ SELECT 'Autor' WHERE NOT EXISTS (SELECT 1 FROM `objekt` WHERE `name` = 'Autor');
 
 INSERT INTO `objekt` (`name`)
 SELECT 'Fahrrad' WHERE NOT EXISTS (SELECT 1 FROM `objekt` WHERE `name` = 'Fahrrad');
+
+-- Überprüfen, ob bereits Daten in der reservierung-Tabelle existieren und nur hinzufügen, wenn sie nicht existieren
+INSERT INTO `reservierung` (`details`, `end_datum`, `start_datum`, `objekt_id`, `person_id`)
+SELECT 'Hausausleihen', '2004-07-18 20:00:00', '2004-07-18 18:00:00', 100, 100
+WHERE NOT EXISTS (SELECT 1 FROM `reservierung` WHERE `details` = 'Hausausleihen' AND `person_id` = 100 AND `objekt_id` = 100);
 
 
 
